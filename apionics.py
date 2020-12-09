1411a05089e732bfcb4dedb9f7bc96f939894f2e
Jeff <jeff@puffergas.com>
2020-12-09 11:39:35 -0500

    Remove GUI, command line only, start artwork.

--- apionics.py
+++ apionics.py
@@ -1,4 +1,3 @@
-import PySimpleGUI as sg
 from telnet import FlightGear
 import sys
 import piplates.TINKERplate as TINK
@@ -25,88 +24,90 @@ import time
 # Boston, MA 02111-1307 USA
 
 
-sg.theme('SystemDefault')
+#-------------------
+# Variables
+#-------------------
 
+active = True
+
+
+#-------------------
+# Functions
+#-------------------
+
+def int_apionics():
+    # Bank (roll_deg), channel 8, servo control, settle time
+    TINK.setMODE(0,8,'servo')
+    time.sleep(1.0)
+    # Variometer, channel 1, servo control, settle time
+    TINK.setMODE(0,1,'servo')
+    time.sleep(1.0)
+    # Fuel gauge, channel 2, servo control, settle time
+    TINK.setMODE(0,2,'servo')
+    time.sleep(1.0)
+
+
+def run_apionics():
+    #loop forever
+    while active:
+        # Horizon, 1/2
+        roll_deg = fg['/instrumentation/attitude-indicator/indicated-roll-deg']
+        # Reverse and calibrate, FG is 45 0 -45, PiPlate is 0 180deg
+        roll_deg_cal = 90.0 - roll_deg
+        # Travel snubber
+        if roll_deg_cal > 180:
+            roll_deg_cal = 180
+        if roll_deg_cal < 0:
+            roll_deg_cal = 0
+        TINK.setSERVO(0,8,roll_deg_cal)
+
+
+        # Variometer, climb rate
+        variometer = fg['/instrumentation/vertical-speed-indicator/indicated-speed-fpm']
+        variometer_deg_cal = 90 + (variometer * 0.045)
+        # Travel snubber
+        if variometer_deg_cal > 180:
+            variometer_deg_cal = 180
+        if variometer_deg_cal < 0:
+            variometer_deg_cal = 0
+        TINK.setSERVO(0,1,variometer_deg_cal)
+
+        # Fuel gauge
+        fuel_gauge = fg['/consumables/fuel/tank/level-gal_us']
+        fuel_gauge_deg_cal = fuel_gauge * 0.9
+        # Travel snubber
+        if fuel_gauge_deg_cal > 180:
+            fuel_gauge_deg_cal = 180
+        if fuel_gauge_deg_cal < 0:
+            fuel_gauge_deg_cal = 0
+        TINK.setSERVO(0,2,fuel_gauge_deg_cal)
+
+
+#-------------------
+# Core
+#-------------------
+
+print("Welcome to apionics")
+time.sleep(3.0)
+
+print("Tring to connect to FlightGear")
+time.sleep(2.0)
 
 try:
     # fg = FlightGear('localhost', 5401)
     fg = FlightGear('192.168.42.7', 5401)
 except ConnectionRefusedError:
-    sg.popup_error('Opps', 'Looks like you started PiStack before FlightGear'
-    ' or did not configure FlightGear - PiStack will exit.', font=(any, 14))
+    print("Telnet Error; aPionics could not connect to FlightGear.")
+    time.sleep(4.0)
     sys.exit()
 
+print("aPionics has connected to FlightGear")
+time.sleep(2.0)
 
-# Value, (double)
-# roll_deg = fg['/instrumentation/attitude-indicator/indicated-roll-deg']
-# variometer = fg['/instrumentation/vertical-speed-indicator/indicated-speed-fpm']
-
-
-reply = sg.popup_ok_cancel('Do you wish to continue?', font=(any, 14))
-print(reply)
-
-if reply == 'Cancel':
-    sys.exit()
-
-
-# Bank (roll_deg), channel 8, servo control, settle time
-TINK.setMODE(0,8,'servo')
+print("Initializing avionic istrumentation")
+int_apionics()
+print("Initialized")
 time.sleep(1.0)
 
-# Variometer, channel 7, servo control, settle time
-TINK.setMODE(0,1,'servo')
-time.sleep(1.0)
-
-# Fuel gauge
-TINK.setMODE(0,2,'servo')
-time.sleep(1.0)
-
-
-#loop forever
-while(True):
-    # Horizon, 1/2
-    roll_deg = fg['/instrumentation/attitude-indicator/indicated-roll-deg']
-    #print(roll_deg)
-    
-    # Reverse and calibrate, FG is 45 0 -45, PiPlate is 0 180deg
-    roll_deg_cal = 90.0 - roll_deg
-
-    # Travel snubber
-    if roll_deg_cal > 180:
-        roll_deg_cal = 180
-    if roll_deg_cal < 0:
-        roll_deg_cal = 0
-
-    TINK.setSERVO(0,8,roll_deg_cal)
-    time.sleep(0.2)
-
-
-    # Variometer, climb rate
-    variometer = fg['/instrumentation/vertical-speed-indicator/indicated-speed-fpm']
-    #print(variometer)
-    
-    variometer_deg_cal = 90 + (variometer * 0.045)
-
-    # Travel snubber
-    if variometer_deg_cal > 180:
-        variometer_deg_cal = 180
-    if variometer_deg_cal < 0:
-        variometer_deg_cal = 0
-
-    TINK.setSERVO(0,1,variometer_deg_cal)
-    time.sleep(0.2)
-
-    # Fuel gauge
-    fuel_gauge = fg['/consumables/fuel/tank/level-gal_us']
-    print(fuel_gauge)
-    
-    fuel_gauge_deg_cal = fuel_gauge * 0.9
-
-    # Travel snubber
-    if fuel_gauge_deg_cal > 180:
-        fuel_gauge_deg_cal = 180
-    if fuel_gauge_deg_cal < 0:
-        fuel_gauge_deg_cal = 0
-
-    TINK.setSERVO(0,2,fuel_gauge_deg_cal)
-    time.sleep(0.2)
+print("Linking your instrumentation")
+run_apionics()
